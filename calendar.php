593898c98d2f998b64274ba59a8c16c580d11671
Nikolas <it-nikola@mail.ru>
2018-09-04 18:04:59 +0300

    Добавление версии 2: изменение обновления календаря с использованием js, разделение компонентов на отдельные файлы, внедрение именования по системе БЭМ, перенос оформления на css, добавление проверки дня сегодняшнего дня  и наличие связи с сервром

--- calendar.php
+++ calendar.php
@@ -1,4 +1,9 @@
 <?
+if($_GET['code'] == 'isonline'){
+	header("Access-Control-Allow-Origin: *");
+	die();
+}
+
 function ShowMon($event)
 {
     $max_day = date("t", mktime(0, 0, 0, $mon, 1, $year));
@@ -8,14 +13,14 @@ function ShowMon($event)
     for ($i = 1, $x = 1; $i <= 35; $i++) {
         $day = date('j', mktime(0, 0, 0, date('n'), date('j') - $w_day + $i));
         $mon = date('n', mktime(0, 0, 0, date('n'), date('j') - $w_day + $i));
-        echo "<td align='center' class='";
-        if (($mon == date('n')) and ($day == date('j'))) //если день и месяц соотв сегодня - помечаем
-        {
-            echo " today ";
-        }
-        if ($mon < date("n")) echo "aftermon"; //  не текущий месяц помечаем
-        if ($mon > date("n")) echo "beforemon"; //  не текущий месяц помечаем
+        echo "<td class='calendar__day ";
+        if ($mon < date("n")) echo "calendar__aftermon"; //  не текущий месяц помечаем
+        if ($mon > date("n")) echo "calendar__beforemon"; //  не текущий месяц помечаем
         echo "'><div class='";
+		if (($mon == date('n')) and ($day == date('j'))) //если день и месяц соотв сегодня - помечаем
+		{
+			echo " calendar__day__today ";
+		}
         foreach ($event as $item) {
             ///////// разбираем дату по частям на месяц и день, а если первый 0, от отрезаем его
             $item_mon = substr($item, 4, 2);
@@ -28,7 +33,7 @@ function ShowMon($event)
             }
             if ($mon == $item_mon and $day == $item_day)//если день и месяц равен item то помечаем его 
             {
-                echo ' active ';
+                echo ' calendar__day__active ';
             }
         }
         echo " '>";
@@ -64,50 +69,39 @@ foreach($arr as $item)
         if(substr($date,0,4) == date("Y")) $arr_event[]=$date;
     }
 }
+
 ?>
-<!DOCTYPE>
-<html>
-<head>
-    <title>Календарь</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta content="text/html; charset=utf-8">
-    <script type="text/javascript">
-        //скрипт часов
-        function digitalWatch() {
-            var date = new Date();
-            var hours = date.getHours();
-            var minutes = date.getMinutes();
-            var seconds = date.getSeconds();
-            if (hours < 10) hours = "0" + hours;
-            if (minutes < 10) minutes = "0" + minutes;
-            if (seconds < 10) seconds = "0" + seconds;
-            document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
-            setTimeout("digitalWatch()", 1000);
-        }
-    </script>
-</head>
-<body onload="digitalWatch()">
-<table align="left" border="0" cellspacing="0" cellpadding="0" width="300px">
-    <tr align="left" valign="top">
-        <th colspan="7"><h1 id="digital_watch" ></h1><?//часы?></th>
-    </tr>
-    <tr align="left" valign="top">
-        <td colspan="5">
-            <h1 class="date1">
-                <div class="moon" title="<?=$day_moon?> лунный день">
-                    <div class="ring"></div>
-                </div><?echo date('j');?>
-            </h1>
-            <h1 class="date2"><?echo $arr_mon_name[date('n')-1];?></h1>
-            <h1 class="date3"><?echo $arr_day_name[date('w')];?></h1>
-        </td>
-        <th colspan="2">
-            <div class="weater" title="Обновлено:  <?="\n".date('j.n.y G:i')?>">
-                <div class="quadr"></div>
-            </div>
-        </th>
-    </tr>
-    <tr align="center" valign="top">
+<div class="header">
+	<h1 id="digital_watch" class="header__watch"></h1>
+	<div class="header__left moon" id="moonBox">
+		<img id = "moon" src = "img/moon<?=$day_moon?>.png" 
+			class="moon__image">
+		<p class="moon__text" id="moonText">
+			<?=$day_moon?> лунный день
+		</p>
+	</div>
+	<div class="header__center date">
+		<p class="date__number">
+		   <?echo date('j');?>
+		</p>
+		<p class="date__month">
+			<?echo $arr_mon_name[date('n')-1];?>
+		</p>
+		<p class="date__name">
+			<?echo $arr_day_name[date('w')];?>
+		</p>
+	</div>
+	<div class="header__right">
+		<div class="weather" 
+				style="background: #ececec url(https://info.weather.yandex.net/2/3.ru.png) -19px -32px;" 
+				title="Обновлено:  <?="\n".date('j.n.y G:i')?>">
+			<div class="weather__border"></div>
+		</div>
+	</div>
+
+</div>
+<table class="calendar">
+    <tr>
         <?
         $arr_w = array('ПН','ВТ','СР','ЧТ','ПТ','СБ','ВС');
         foreach($arr_w as $w_item)
@@ -115,73 +109,12 @@ foreach($arr as $item)
             echo "<th> $w_item </th>";}
         ?>
     </tr>
-    <tr align="center" valign="top">
+    <tr>
         <? ShowMon($arr_event);?>
     </tr>
 </table>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        text-align: center;    font-family: 'MS Sans Serif', Geneva, sans-serif;;
-    }
-    td {
-        padding: 2px;width: 40px;
-    }
-    .today {
 
-        padding: 0px;
-    }
-    .today div {
-        border: 6px solid #02e002;
-        margin: -4px;position: relative;
-    }
-    .aftermon {
-        background: #ccc;
-        color: #fff;
-    }
-    .active {
-        background: red;
-    }
-    .aftermon .active, .beforemon .active {
-        background: #f99;
-    }
-    .beforemon {
-        background: #f5f5f5;
-        color: #9a9a9a;
-    }
-    .date2 {
-        font-size: 25px;
-    }
-    .date3 {
-        font-size: 25px;
-        font-weight: normal;
-    }
-    .weater, .quadr{
-        width: 80px;
-        height: 80px;    margin: 10px 0px;
-    }
-    .weater{
-        background: #ececec url(https://info.weather.yandex.net/2/4.ru.png?domain=ru) -35px -31px;
-    }
-    .quadr{
-        background: url(quadr.png) 0 0;
-        margin:0;
-    }
-    h1.date1 {
-        float: left;width: 180px;
-    }
-    .moon, .ring{
-        float: left;
-        width: 50px;
-        height: 50px;
-    }
-    .moon{
-        background-image: url(<?=$url_moon?>);
-    }
-    .ring{
-        background-image: url(ring.png);
-    }
+<style>
+
 </style>
-</body>
-</html>
+
---
+++ widget/widget.js
@@ -0,0 +1,167 @@
+﻿'use strict';
+var winWidth = 304; // ширина окна
+var winHeight = 314; // высота окна
+// изменяем размер
+window.resizeTo(winWidth, winHeight);
+
+// окно в центр экрана
+var winPosX = screen.width - winWidth;
+var winPosY = screen.Height - winHeight;
+window.moveTo(winPosX, winPosY);
+var server = "http://widget/calendar.php"; 		// адрес сервера
+var status = 'offline'; // статус сети
+var reload = 15;        // период обновления статуса
+var pingTimer;          // таймер пинга сети
+var stepOnline = 60 * 1000;  // шаг проверки сети если онлайн
+var stepOffline = 500;  // шаг проверки сети если оффлайн
+var pingCount = 0;      // счетчик запросов статуса
+var frame = null;              // фрейм виджета
+var curtain;              // блок заглушка
+var today;
+
+function ajax(url,param,method) {
+	var strParam;
+	if(param === undefined) strParam = '';
+	else if (typeof(param) == "object") {
+		strParam = "?";
+		for (var key in param){
+			if(strParam.length > 3) strParam += '&';
+			strParam += key + "=" + param[key];
+			
+		}
+	}
+	if(method === undefined) method = 'GET';
+	var request = new XMLHttpRequest();
+	request.open(method, url + strParam, false);
+	try {
+		request.send();
+			return request;
+	} catch (e) {
+		return false;
+	}
+}
+
+/** функция определения статуса
+* вернет истину только если получит нормальный заголовок
+**/
+function appIsOnline(){
+	var x = ajax(server,{'code':'isonline'},'HEAD');
+	if (x === false) return false;
+	if (x.status >= 200 && x.status < 304) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * функция добавления/обновления календаря
+ */
+function calendarLoad(){
+	var x = ajax(server);
+	if(x.status == 200){
+		calendarBox(x.responseText);
+	}else{
+		status = 'offline';
+		widget();
+	}
+}
+
+/**
+ * Функция размещения календаря на странице
+ */
+function calendarBox(content) {
+	if(frame === null){
+		frame = document.getElementById('calendar');
+	}
+	today = new Date();
+	frame.innerHTML = content;
+	curtain.style.display = 'none';
+	digitalWatch();
+	moonBox();
+}
+
+/**
+ * Функция основного приложения
+ * 1. проверить есть ли сеть
+ * (+)
+ *     1. Загрузить/перезагрузить календарь
+ *     2. Проверять сеть с шагом stepOnline
+ *        и обновить календарь через reload раз
+ * (-) 1. Включить заставку
+ *     2. Изменить шаг проверки сети на stepOffline
+ */
+function widget(){
+	if(appIsOnline() === true){
+		if(status === 'offline'){
+			status = 'online';
+			calendarLoad();
+			pingCount = 0;
+		}
+		if(pingCount >= reload){
+			calendarLoad();
+			pingCount = 0;
+		}else{
+			pingCount++;
+		}
+		pingTimer = setTimeout(widget,stepOnline);
+	}
+	else{
+		if(status === 'online'){
+			status = 'offline';
+			curtain.style.display = 'block';
+			clearTimeout(pingTimer);
+			pingCount = 0;
+		}
+		pingTimer = setTimeout(widget, stepOffline);
+	}
+}
+
+/**
+ * функция проверки текущей даты
+ * перезагрузит календарь если дата в календаре не текущая
+ */
+function noToday(date) {
+	if (today.getDate() != date.getDate()) {
+		status = 'offline';
+		curtain.style.display = 'block';
+		clearTimeout(pingTimer);
+		pingCount = 0;
+		widget();
+	}
+}
+
+/**
+ *
+ */
+function digitalWatch() {
+	var date = new Date();
+	noToday(date);
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	if (hours < 10) hours = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+	document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
+	setTimeout("digitalWatch()", 1000);
+}
+
+function moonBox(){
+	var box  = document.getElementById('moonBox');
+	var img  = document.getElementById('moon');
+	var text = document.getElementById("moonText");
+	box.onmouseover = function () {
+		img.style.display = 'none';
+		text.style.display = 'block';
+	}
+	box.onmouseout = function () {
+		img.style.display = 'block';
+		text.style.display = 'none';
+	}
+}
+
+window.onload=function(){
+	curtain = document.getElementById('block');
+	widget();
+};
