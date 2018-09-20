a60270589aff57b4e08496e9ff6eb71344bb1655
Nikolas <it-nikola@mail.ru>
2018-09-20 17:32:59 +0300

    + табуляция

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ if(!empty($_GET['code'])):
     header("Access-Control-Allow-Origin: *");
     header("Cache-Control: no-cache");
     if ($_GET['code'] == 'e3sueck4k'){
-    $urlMoon = 'https://rot.lyna.info/rotator/preview/?type_id=1&city_id=303';
-    $urlEvents='https://calendar.google.com/calendar/ical/udidbi4ns794kt5eukbvtghbig%40group.calendar.google.com/public/basic.ics';
-    $file = file_get_contents($urlMoon);
-    preg_match("#\/([\d]{1,2})\.png#s", $file, $moon);
-    $file = file_get_contents($urlEvents);
-    preg_match_all("#START:(\d+)#s",$file,$events);
-    echo "moon".$moon[1]." ".join(" ",$events[1]);
+        $urlMoon = 'https://rot.lyna.info/rotator/preview/?type_id=1&city_id=303';
+        $urlEvents='https://calendar.google.com/calendar/ical/udidbi4ns794kt5eukbvtghbig%40group.calendar.google.com/public/basic.ics';
+        $file = file_get_contents($urlMoon);
+        preg_match("#\/([\d]{1,2})\.png#s", $file, $moon);
+        $file = file_get_contents($urlEvents);
+        preg_match_all("#START:(\d+)#s",$file,$events);
+        echo "moon".$moon[1]." ".join(" ",$events[1]);
     }
     else
         die();
